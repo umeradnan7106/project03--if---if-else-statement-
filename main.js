69333837db33f1f9fb4e9a1de9bfd2e2039e2674
umeradnan7106 <120960218+umeradnan7106@users.noreply.github.com>
2024-07-10 03:55:41 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+//       if statement
+let isRainning = false;
+if (isRainning) {
+    console.log("wear a raincoat");
+}
+else {
+    console.log("wear a sunglasses");
+}
+//        if-else statement
+let weather = "cloudy";
+if (weather == "raining") {
+    console.log("wear a raincoat");
+}
+else if (weather == "sunny") {
+    console.log("wear a normal dress");
+}
+else if (weather = "cloudy") {
+    console.log("wear a light jacket");
+}
+else {
+    console.log("wear a sunglasses");
+}
+let isHungry = false;
+// ? represent if and  : represent else
+let snack = isHungry ? "you eat apple" : "drink the water";
+console.log(snack);
+let myAge = 18;
+let mycountry = "Pakistan";
+// let enterInSwing = myAge >= 18 ? "You are allowed" : "you are not allowed";
+// console.log(enterInSwing);
+if (myAge >= 18) {
+    if (mycountry = "Paksitan") {
+        console.log("Here is your ticket");
+    }
+    else {
+        console.log("You are not allowed");
+    }
+}
+let number = 10;
+let work = number <= 10 ? "you can read books" : "you can do your home work";
+console.log(work);
